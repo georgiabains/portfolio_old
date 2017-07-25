28ece36362e782025a3a47876ef0f607ce19fa27
georgiabainsdesign <georgiabains@hotmail.co.uk>
2017-07-24 22:48:42 -0400

    entire webpage

---
+++ sesame/gulpfile.js
@@ -0,0 +1,81 @@
+// from tse work
+
+var gulp = require('gulp');
+var less = require('gulp-less');
+var browserSync = require('browser-sync').create();
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
+
+// Compile LESS files from /less into /css
+gulp.task('less', function() {
+    return gulp.src('less/styles.less')
+        .pipe(less())
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+gulp.task('lessOnPage', function() {
+    return gulp.src('less/pages.less')
+        .pipe(less())
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// Minify compiled CSS
+gulp.task('minify-css', ['less'], function() {
+    return gulp.src('css/styles.css', 'css/pages.css')
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+gulp.task('minify-css', ['lessOnPage'], function() {
+    return gulp.src('css/pages.css')
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// Minify JS
+gulp.task('minify-js', function() {
+    return gulp.src('js/scripts.js')
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('js'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// Run everything
+gulp.task('default', ['less', 'minify-css', 'minify-js']);
+
+// Configure the browserSync task
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: ''
+        },
+    })
+})
+
+// Dev task with browserSync
+gulp.task('serve', ['browserSync', 'less', 'minify-css', 'minify-js'], function() {
+    gulp.watch('less/*.less', ['less']);
+    gulp.watch('css/*.css', ['minify-css']);
+    gulp.watch('js/*.js', ['minify-js']);
+    // Reloads the browser whenever HTML or JS files change
+    gulp.watch('*.html', browserSync.reload);
+    gulp.watch('js/**/*.js', browserSync.reload);
+});
---
+++ sesame/js/scripts.js
@@ -0,0 +1,111 @@
+// show/hide content
+$(document).ready(function(){
+    
+    /*main tabs*/
+    $(".projectclick").click(function(){
+        $(".projects").fadeIn("");
+        $(".intro").hide("");
+        $(".process").hide("");
+        $(".about").hide("");
+        $(".contact").hide("");
+        $(".credits").hide("");
+    });
+    
+    $(".processclick").click(function(){
+        $(".process").fadeIn("");
+        $(".intro").hide("");
+        $(".projects").hide("");
+        $(".about").hide("");
+        $(".contact").hide("");
+        $(".credits").hide("");
+    });
+    
+    $(".aboutclick").click(function(){
+        $(".about").fadeIn("");
+        $(".intro").hide("");
+        $(".process").hide("");
+        $(".projects").hide("");
+        $(".contact").hide("");
+        $(".credits").hide("");
+    });
+    
+    $(".contactclick").click(function(){
+        $(".contact").fadeIn("");
+        $(".intro").hide("");
+        $(".process").hide("");
+        $(".about").hide("");
+        $(".projects").hide("");
+        $(".credits").hide("");
+    });
+    
+    $(".creditclick").click(function(){
+        $(".credits").fadeIn("");
+        $(".intro").hide("");
+        $(".process").hide("");
+        $(".about").hide("");
+        $(".projects").hide("");
+        $(".contact").hide("");
+    });
+    
+    // icons in process
+    $(".iconclick").click(function(){
+        $(this).next(".step").fadeToggle("average");
+    }); 
+    
+    $(".iconclick_r").click(function(){
+        $(this).next(".step_r").fadeToggle("average");
+    }); 
+        
+    // project navigation
+    $(".newlantisclick").click(function(){
+        $(".newlantis").fadeIn("");
+        $(".tse").fadeOut("");
+        $(".spotlight").fadeOut("");
+    });
+    
+    $(".tseclick").click(function(){
+        $(".tse").fadeIn("");
+        $(".newlantis").fadeOut("");
+        $(".spotlight").fadeOut("");
+    });
+    
+    $(".spotlightclick").click(function(){
+        $(".spotlight").fadeIn("");
+        $(".newlantis").fadeOut("");
+        $(".tse").fadeOut("");
+    });
+    
+});
+
+/*smooth scroll to target link*/
+$(document).ready(function(){
+        $('a[href^="#"]').on('click',function (e) {
+            e.preventDefault();
+ 
+            var target = this.hash,
+            $target = $(target);
+ 
+            $('html, body').stop().animate({
+                'scrollTop': $target.offset().top
+            }, 900, 'swing', function () {
+                window.location.hash = target;
+            });
+        });
+});
+
+/**
+ * @name		Shuffle Letters
+ * @author		Martin Angelov
+ * @version 	1.0
+ * @url			http://tutorialzine.com/2011/09/shuffle-letters-effect-jquery/
+ * @license		MIT License
+ */
+$(document).ready(function(){
+    var container = $(".welcome");
+    
+    container.shuffleLetters("");
+    
+});
+
+
+    
